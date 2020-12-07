e2f51db1835ac244b28b37ed28f59e8ae5511df6
Lucas <lucasmv99@hotmail.com>
2020-12-06 22:45:44 -0300

    Add files via upload

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,12 +1,9 @@
 
-// create map
 const map = L.map('mapid').setView([-29.9293999,-51.0390298], 16)
 
-// create and add tileLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map)
-// create icon for
 
 const icon = L.icon({
   iconUrl: "public/images/map-marker.svg",
@@ -15,7 +12,6 @@ const icon = L.icon({
   popupAnchor: [170, 2]
 })
 
-// create popup overlay
 const popup = L.popup({
   closeButton: false,
   className: "map-popup",
@@ -24,7 +20,6 @@ const popup = L.popup({
 }).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg "> </a>')
 
 
-// create and add marker
 L
 .marker([-29.9293999,-51.0390298], { icon })
 .addTo(map)
